28b40782598ce29d89169edb590b62d23ec88935
sho-luv <sho.luv@gmail.com>
2023-05-08 13:19:20 -0700

    fixed some calulations errors

--- hashstat.py
+++ hashstat.py
@@ -86,6 +86,7 @@ parser.add_argument("-d", "--details", action="store_true", help="show all detai
 parser.add_argument("-c", "--cracked", action='store_true', help='show top ten')
 parser.add_argument("-lm", action='store_true', help='show top ten')
 parser.add_argument("-nt", action='store_true', help='show top ten')
+parser.add_argument("-e", action='store_true', help='show identical user/pass')
 
 #group = parser.add_mutually_exclusive_group()
 group = parser.add_argument_group('Additional options')
@@ -107,18 +108,29 @@ def main():
         rids = []
         nt_hashes = []
         lm_hashes = []
+        lm_total = []
+        lm_count = []
+        nt_total = []
         ntds = []
         details = False
         top_hash = False
         top_cracked = False
         filename = ""
         file_data = ""
+        machine_accounts = 0
+        nt_stats = []
+        lm_stats = []
+        nt_max = 0
+        lm_max = 0 
 
     my_ntds = ntds()
     my_ntds.details = options.details
     my_ntds.top_hash = options.top
     my_ntds.top_cracked = options.cracked
     my_ntds.filename = options.ntds
+    my_ntds.lm = options.lm
+    my_ntds.nt = options.nt
+
 
     # try to open file with hashes
     try:
@@ -136,6 +148,7 @@ def main():
 
     print(CYAN+"\nThe are the statistics based on the pwdump file"+NOCOLOR)
     print(CYAN+"These statistics exclude machine accounts."+NOCOLOR)
+    print(CYAN,my_ntds.machine_accounts, "machine accounts were removed from these stats."+NOCOLOR)
     print_ntds_stats(my_ntds)
 
 def parse_ntds(ntds):
@@ -151,90 +164,105 @@ def parse_ntds(ntds):
                 ntds.nt_hashes.append(x[3])
                 # t.ly/YmZgE
                 #ntds.ntds.append({"user":x[0], "rid":x[1], "lm":x[2], "nt":x[3]})
+            else:
+                ntds.machine_accounts += 1
+    # pipal style get dup values of list
+    ntds.nt_stats, ntds.nt_total, ntds.nt_max = get_top_ten_reused_hashes(ntds.nt_hashes)
+    ntds.lm_stats, ntds.lm_total, ntds.lm_max = get_top_ten_reused_hashes(ntds.lm_hashes)
+
+def print_nt_stats(ntds):
+    print(LIGHTGREEN+"\n[+] "+NOCOLOR, end = '')
+    print("Total account NT hashes:",end='')
+    print(RED,len(ntds.nt_hashes),NOCOLOR)
+
+    print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
+    print("Total unique NT hashes:",end='')
+    print(RED,len(list(set(ntds.nt_hashes))),NOCOLOR)
+
+    print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
+    print("Most reused NT hash was used:",end='')
+    print(RED,ntds.nt_max,NOCOLOR)
+
+    print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
+    print("Total # of duplicate NT hashes:",end='')
+    print(RED,ntds.nt_total,NOCOLOR)
+    print()
+
+def print_lm_stats(ntds):
+    print(LIGHTGREEN+"\n[+] "+NOCOLOR, end='')
+    print("Total accounts with LM hashes:",end='')
+    print(RED,len(ntds.lm_hashes),NOCOLOR)
+
+    get_lm_count(ntds)
+    print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
+    print("Total LM hashes:",end='')
+    print(RED,len((ntds.lm_count)),NOCOLOR)
+
+    print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
+    print("Total unique LM hash passwords:",end='')
+    print(RED,(len(set(ntds.lm_count))),NOCOLOR)
+
+    print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
+    print("Most reused LM hash password was used:",end='')
+    print(RED,ntds.lm_max,NOCOLOR)
+    print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
+    print("Total # of duplicate LM hashes:",end='')
+    print(RED,ntds.lm_total,NOCOLOR)
+    print()
+
+def print_nt_top_ten(ntds):
+
+    print(GREEN,"Top Ten duplicate NT hashes:",NOCOLOR)
+    print(YELLOW+"-------------------------------------------------"+NOCOLOR)
+    print_top_ten(ntds.nt_stats, len(ntds.nt_hashes))
+
+def print_lm_top_ten(ntds):
+
+    print(GREEN,"Top Ten duplicate LM hashes:",NOCOLOR)
+    print(YELLOW+"-------------------------------------------------"+NOCOLOR)
+    print_top_ten(ntds.lm_stats, len(ntds.lm_hashes))
 
 def print_ntds_stats(ntds):
 
     # NT Password Hashes Breakdown #==================================================
-    if ntds.nt_hashes:
-        print(LIGHTGREEN+"\n[+] "+NOCOLOR, end = '')
-        print("Total NT hashes:",end='')
-        print(RED,len(ntds.nt_hashes),NOCOLOR)
+    if ntds.nt == True:
+        if ntds.nt_hashes:
+            print_nt_stats(ntds)
 
-        print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
-        print("Total unique NT hashes:",end='')
-        print(RED,len(list(set(ntds.nt_hashes))),NOCOLOR)
+            if ntds.top_hash:
+                print_nt_top_ten(ntds)
 
-        stats, nt_total, nt_max = get_top_ten_reused_hashes(ntds.nt_hashes)
-        print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
-        print("Most reused NT hash was used:",end='')
-        print(RED,nt_max,NOCOLOR)
-        print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
-        print("Total # of duplicate NT hashes:",end='')
-        print(RED,nt_total,NOCOLOR)
-        print()
-
-        # NT Top Hashes Breakdown #===================================================
-        if ntds.top_hash:
-            print(GREEN,"Top Ten duplicate NT hashes:",NOCOLOR)
-            print(YELLOW+"-------------------------------------------------"+NOCOLOR)
-            print_top_ten(stats, len(ntds.nt_hashes))
-
-        # NT Passwords Cracked Hashes Breakdown #=====================================
-        if ntds.top_cracked:
-            #print("Trying to see Cracked shit!")
-            cracked_hash_stats(ntds.nt_hashes,"nt",ntds.filename)
+            if ntds.top_cracked:
+                cracked_hash_stats(ntds.nt_hashes,"nt",ntds.filename, ntds.nt_hashes)
 
     # LM Password Hashes Breakdown #==================================================
-    if ntds.lm_hashes:
-        print(LIGHTGREEN+"[+] "+NOCOLOR, end='')
-        print("Total accounts with LM hashes:",end='')
-        print(RED,len(ntds.lm_hashes),NOCOLOR)
+    if ntds.lm == True:
+        if ntds.lm_hashes:
+            print_lm_stats(ntds)
 
-        lm_total = []
-        
+            if ntds.top_hash:
+                print_lm_top_ten(ntds)
+
+            if ntds.top_cracked:
+                cracked_hash_stats(ntds.lm_hashes,"lm",ntds.filename,ntds.lm_count)
+        else:
+            print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
+            print("Found no LM hashes in this file\n")
+
+def get_lm_count(ntds):
+        # how to count lm hashes like john does
+        # cut -d: -f3 lm.hashes | fold -w 16 | grep -v aad3b435b51404ee |wc
         for s in ntds.lm_hashes:
             s1 = s[:len(s)//2]
             s2 = s[len(s)//2:]
-            lm_total += [s1]
-            lm_total += [s2]
-            myset = set(lm_total)
+            ntds.lm_count += [s1]
+            ntds.lm_count += [s2]
             # .discard() only removed if value exist vs .remove() which rasies error
-            myset.discard("aad3b435b51404ee") 
-            myset.discard("AAD3B435B51404EE") 
+            for value in ntds.lm_count:
+                if value == 'aad3b435b51404ee':
+                    ntds.lm_count.remove('aad3b435b51404ee')
 
-        # Printing output
-        print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
-        print("Total LM hashes:",end='')
-        print(RED,len(list(set(myset))),NOCOLOR)
-
-        print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
-        print("Total unique LM hash passwords:",end='')
-        print(RED,len(list(set(ntds.lm_hashes))),NOCOLOR)
-
-        # pipal style get dup values of list
-        stats, lm_total, lm_max = get_top_ten_reused_hashes(ntds.lm_hashes)
-
-        print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
-        print("Most reused LM hash password was used:",end='')
-        print(RED,lm_max,NOCOLOR)
-        print(LIGHTGREEN+"[+] "+NOCOLOR,end='')
-        print("Total # of duplicate LM hashes:",end='')
-        print(RED,lm_total,NOCOLOR)
-        print()
-
-        if ntds.top_hash:
-            print(GREEN,"Top Ten duplicate LM hashes:",NOCOLOR)
-            print(YELLOW+"-------------------------------------------------"+NOCOLOR)
-            print_top_ten(stats, len(ntds.lm_hashes))
-
-        if ntds.top_cracked:
-            cracked_hash_stats(ntds.lm_hashes,"lm",ntds.filename)
-    else:
-        print(LIGHTGREEN+"[+] "+NOCOLOR, end = '')
-        print("Found no LM hashes in this file\n")
-
-def cracked_hash_stats(hashes, type_hash, filename):
-    # This function takes a list 
+def cracked_hash_stats(hashes, type_hash, filename, hash_total):
 
     john_cracked = []
     domains = []
@@ -242,20 +270,13 @@ def cracked_hash_stats(hashes, type_hash, filename):
     dups = []
     hash_format = "--format="+type_hash
 
-    # get cracked hashes from john the ripper
-    #process_john = subprocess.Popen(('john', '--show', hash_format, "/tmp/hashstat"),
-    #        stdout=subprocess.PIPE, shell=False)
-    process_john = subprocess.Popen(('john', '--show', hash_format, filename), 
-            stdout=subprocess.PIPE, shell=False)
-    output = subprocess.check_output(('grep', ':::'), 
-            stdin=process_john.stdout, shell=False).decode("utf-8")
-    process_john.stdout.close()
+    process_john = subprocess.check_output(['john', '--show', hash_format, filename])
+    john_cracked = (process_john).decode("utf-8").split('\n')
+    while("" in john_cracked):
+        john_cracked.remove("")
+    total_cracked = (john_cracked.pop().split(" ")[0])
 
-    # clean ntds
-    # convert strings to list
-    john_cracked = output.split('\n')
-    # delete empty list items
-    john_cracked = [ x for x in john_cracked if x ]
+    john_cracked = [ x for x in john_cracked if x ]     # delete empty list items
 
     # if lm hashes delete empty lm hashes
     if type_hash == "lm":
@@ -269,6 +290,8 @@ def cracked_hash_stats(hashes, type_hash, filename):
         if '\\' not in item:
             domain = ""
             username, password, rid, lm, nt = item.split(':', 4)
+            if "aad3b435b51404ee" == item:
+                print(item)
             passwords.append(password)
             if username.lower() == password.lower():
                 dups.append(username)
@@ -298,12 +321,16 @@ def cracked_hash_stats(hashes, type_hash, filename):
     print("Unique number of domains found in ntds file:",end='')
     print(RED,len(domains),NOCOLOR)
 
+   
+    print("\nDuplicated hashes (username equals password):")
+    for dup in dups:
+        print(dup)
+
     # print cracked top ten stats
     print()
     print(GREEN,"Top 10 Cracked",type_hash.upper(),"hashes",NOCOLOR)
     print(YELLOW+"--------------------------"+NOCOLOR)
     print_top_ten(pw_stats,len(hashes))
-
 
 def print_top_ten(my_list, total):
     for x,y in my_list:
@@ -340,9 +367,6 @@ def get_top_ten_reused_hashes(my_list):
     freq.pop("31d6cfe0d16ae931b73c59d7e0c089c0", None)
     freq.pop("31D6CFE0D16AE931B73C59D7E0C089C0", None)
     hashMax = max(freq.values())
-
-    # freq = {'f0c99bb71ee888f1ebade3ec1090c5f0': 1, '93f6796100b7773d1d71060d896b7a46': 1, '82175fce03b77644417eaf50cfac29c3': 1}
-    # freq is a two value dictionary.
 
     for x,y in sorted(freq.items(), key = lambda kv:(kv[1], kv[0]), reverse=True)[0:10]:
         stats.append([y,x])
